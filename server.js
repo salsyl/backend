95b824eeedce4388c5b3accdbc2ccfd43c37a599
salsyl <47780375+salsyl@users.noreply.github.com>
2019-05-22 11:28:17 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,88 +1,280 @@
 'use strict';
+//////////////////////////////////
+var ipAdresse =
+    '192.168.1.21'
+// '51.83.71.226' 
+// 'localhost'
+;
+
 ////////////mongo/////////////////////////
 const MongoClient = require('mongodb');
-const url = 'mongodb+srv://dbSaloum:dbSaloumpsw@gettingstarted-qdagb.mongodb.net/test';
+// const url = 'mongodb+srv://dbSaloum:dbSaloumpsw@gettingstarted-qdagb.mongodb.net/test';
+const url = 'mongodb://localhost:27017';
 const dbName = 'backend';
-////////////////express//////////////////////////
+
+////////////////express////////////////////////// 
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+//////session 
+const session = require('express-session');
+app.use(session({
+    secret: '123456789SECRET',
+    saveUninitialized: false,
+    resave: false
+}));
 
+/////variable///////////
+var histoPartie;
+var users = {};
+var connectionsLimite = 2;
+var nbJoueur = false;
+var point = false;
+////////////////////
 app.set('view engine', 'pug')
-app.use('/js', express.static(__dirname + '/src/js'))
-app.use('/css', express.static(__dirname + '/src/css'))
-app.use('/img', express.static(__dirname + '/src/img'))
+app.use('/js', express.static(__dirname + '/src/js'));
+app.use('/css', express.static(__dirname + '/src/css'));
+app.use('/img', express.static(__dirname + '/src/img'));
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+//////////la page resultats
+app.get('/pug', function (req, res) {
+    MongoClient.connect(url, {
+        useNewUrlParser: true
+    }, function (err, client) {
+        const db = client.db(dbName);
+        const collection = db.collection('resultat');
+
+        collection.find().toArray(function (err, data) {
+            histoPartie = data;
+            client.close();
+            res.render('lesScores', {
+                title: 'Titre de la page',
+                message: histoPartie
+            });
+        });
+    });
+
 });
-app.get('/carre', function (req, res) {
-    res.sendFile(__dirname + '/exemple5.html')
-});
-app.get('/jeu', function (req, res) {
-    res.render('formulaire.pug')
-});
-///////////socket.io///////////
+
+
+///////////socket.io ///////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
 io.on('connection', function (socket) {
-    console.log('connection a socket io');
+
+    // limite a 2 clients
+    if (io.engine.clientsCount > connectionsLimite) {
+        socket.emit('complet', {
+            message: 'attendez , partie en cours '
+        });
+        return
+    };
+    var me = false;
+
+    console.log('new user ');
+    for (var i in users) {
+        socket.emit('new', users[i]);
+    };
     socket.on('inscription', function (message) {
-        console.log(' message.pseudo');
+
         MongoClient.connect(url, {
             useNewUrlParser: true
         }, function (err, client) {
             const db = client.db(dbName);
             const collection = db.collection('inscrit');
-            //////////////////////////           
+
             collection.find({
                     pseudo: message.pseudo,
-                    psw: message.psw
                 })
                 .toArray(function (err, data) {
-                    console.log(data.length + 'data');
-                    console.log(message.pseudo + ' 1');
+
                     if (data.length > 0) {
-                        console.log(data[0].pseudo + ' deja pris ' + data.length + ' fois');
-                        socket.emit('badUser', 'psw erroné ou use r deja pris !')
+                        socket.emit('dejaPris', )
+
                     } else {
                         collection.insertOne(message);
-                        console.log('insertion ok');
+                        me = message;
+                        me.id = message.pseudo;
+                        me.score = 0;
+                        me.nbJoueur = nbJoueur;
+
+                        ///connection ok
+                        if (io.engine.clientsCount < 2) {
+                            socket.emit('attente', {
+                                message: 'attente de joueur '
+                            });
+                            users[me.id] = me;
+                            io.emit('new', me);
+                            return
+                        } else {
+                            if (io.engine.clientsCount === 2) {
+                                if (nbJoueur) {
+                                    io.emit('new',
+                                        me
+                                    );
+                                    socket.emit('logged');
+                                    users[me.id] = me;
+                                } else {
+                                    io.emit('new',
+                                        me
+                                    );
+                                    socket.emit('attente', {
+                                        me
+                                    });
+                                    users[me.id] = me;
+                                    nbJoueur = true;
+                                }
+                            }
+                        };
+
                     }
                     client.close();
                 });
         });
     });
-    //////////////////////    
+
+    /////////////connection///////:   
     socket.on('formConnect', function (message) {
-        console.log(' message.pseudo');
         MongoClient.connect(url, {
             useNewUrlParser: true
         }, function (err, client) {
             const db = client.db(dbName);
             const collection = db.collection('inscrit');
-            //////////////////////////           
+
             collection.find({
                     pseudo: message.pseudo
                 })
                 .toArray(function (err, data) {
-                    console.log(data.length + 'data');
-                    console.log(message.pseudo + ' 1');
                     if (data.length > 0) {
-                        console.log(data[0].pseudo + ' deja pris ' + data.length + ' fois');
+                        if (data[0].psw === message.psw) {
+                            me = message;
+                            me.id = message.pseudo;
+                            me.score = 0;
+                            me.nbJoueur = nbJoueur;
+
+                            ///connection ok
+                            if (io.engine.clientsCount < 2) {
+                                socket.emit('attente', {
+                                    message: 'attente de joueur '
+                                });
+                                users[me.id] = me;
+                                io.emit('new', me);
+                                return
+                            } else {
+
+                                if (io.engine.clientsCount === 2) {
+                                    if (nbJoueur) {
+                                        io.emit('new',
+                                            me
+                                        );
+
+                                        socket.emit('logged');
+                                        users[me.id] = me;
+
+                                    } else {
+
+                                        io.emit('new',
+                                            me
+                                        );
+
+                                        socket.emit('attente', {
+                                            me
+                                        });
+
+                                        users[me.id] = me;
+                                        nbJoueur = true;
+                                    }
+                                }
+                            };
+
+                        } else {
+
+                            // 'mdp nok'
+                            socket.emit('incorrect', {
+                                message: 'mot de passe incorrect.'
+                            })
+                        }
                     } else {
-                        collection.insertOne(message);
-                        console.log('insertion ok');
+                        // console.log('mdp nok')
+                        socket.emit('incorrect', {
+                            message: 'pseudo ou mot de passe incorrect.'
+                        })
                     }
+
                     client.close();
                 });
         });
     });
-    ////////////////////////:
 
+    ////////////envoi position////////////////// 
+    socket.on('unMouvement', function (message) {
+
+        me.score += 1;
+
+        //////fin de partie /////////
+        if (me.score === 1) {
+            io.emit('score', {
+                info: me,
+            });
+
+            io.emit('perdu', {
+                vainqueur: me,
+                participants: users
+            });
+
+            // demandeResultats au vainqueur
+            socket.emit('demandeResultats');
+
+        } else {
+
+            io.emit('score', {
+                info: me,
+            });
+
+            io.emit('positionPourTousLeMonde', {
+                x: message.x,
+                y: message.y,
+            });
+        };
+    });
+
+    // recuperation du resultat final data.resultatFinal
+    socket.on('resultatFinal', function (data) {
+
+        MongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (err, client) {
+            const db = client.db(dbName);
+            const collection = db.collection('resultat');
+
+            collection.insertOne({
+                historique: data.resultatFinal
+            });
+        });
+    });
     /////////////////////////////
     socket.on('disconnect', function () {
-        console.log('user is déconnected');
+        if (!me) {
+            return false;
+        };
+        if (nbJoueur) {
+            io.emit('deconnectionUser', me);
+            nbJoueur = false;
+
+        }
+        delete users[me.id];
+        socket.disconnect();
+
     })
 });
 
